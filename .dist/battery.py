fd29511c36b34fcd2ed49b1271ed984bec3752e3
Abhinav Nigade <abhinavv.nigade09@gmail.com>
2025-10-15 00:03:27 +0530

    dataset

--- .dist/battery.py
+++ .dist/battery.py
@@ -2,5 +2,18 @@ import pandas as pd
 import numpy as np
 
 file_path = r'C:\Users\Abhinav Nigade\OneDrive\Desktop\AmpRenew\.dist\ev_battery_charging_data.csv'  #change file path
-data = pd.read_csv(file_path)
-print(data.head(5))
+df = pd.read_csv(file_path)
+print(df.head(5))
+
+#print("\nSummary statistics (numeric columns only):\n")
+#summary = df.describe()
+#print(summary)
+df.replace("?", np.nan, inplace = True)
+
+missing_data = df.isnull()
+#print(missing_data.head(5))
+
+for column in missing_data.columns.values.tolist():
+    print(column)
+    print (missing_data[column].value_counts())
+    print("") 
