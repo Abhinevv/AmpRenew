20fe799498eb8ceaba89145d50ab7fc37112a9f6
Abhinav Nigade <abhinavv.nigade09@gmail.com>
2025-10-16 15:06:16 +0530

    regression

--- .dist/battery.py
+++ .dist/battery.py
@@ -1,19 +1,25 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-file_path = r'C:\Users\Abhinav Nigade\OneDrive\Desktop\AmpRenew\.dist\ev_battery_charging_data.csv'  #change file path
+
+file_path = r'C:\Users\Abhinav Nigade\OneDrive\Desktop\AmpRenew\.dist\ev_battery_charging_data.csv'
 df = pd.read_csv(file_path)
 print(df.head(5))
 
-#print("\nSummary statistics (numeric columns only):\n")
-#summary = df.describe()
-#print(summary)
-df.replace("?", np.nan, inplace = True)
+
+df.replace("?", np.nan, inplace=True)
+
 
 missing_data = df.isnull()
-#print(missing_data.head(5))
-
 for column in missing_data.columns.values.tolist():
     print(column)
-    print (missing_data[column].value_counts())
-    print("") 
+    print(missing_data[column].value_counts())
+    print("")
+    break  
+
+# check for factors affecting battery degradation rate
+sns.regplot(x="Efficiency (%)", y="Degradation Rate (%)", data=df)
+plt.ylim(0,)
+plt.show()
